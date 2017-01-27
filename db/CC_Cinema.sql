5eeda2c001533989d34a8689a6bdd209a9cf2f58
3ll3n <ellengrafton01@gmail.com>
2017-01-27 11:13:40 +0000

    set up file structure and created tables

---
+++ db/CC_Cinema.sql
@@ -0,0 +1,21 @@
+DROP TABLE customers CASCADE;
+DROP TABLE films CASCADE;
+DROP TABLE tickets;
+
+CREATE TABLE customers(
+  id SERIAL8 primary key,
+  name VARCHAR(255),
+  funds INT8
+);
+
+CREATE TABLE films(
+  id SERIAL8 primary key,
+  title VARCHAR(255),
+  price INT8
+);
+
+CREATE TABLE tickets(
+  ID SERIAL8 primary key,
+  customer_id INT8 REFERENCES customers(id) ON DELETE CASCADE,
+  film_id INT8 REFERENCES films(id) ON DELETE CASCADE
+);
