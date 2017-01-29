ab9f41894c74923ca0305e87cd98f61a89cf51d5
3ll3n <ellengrafton01@gmail.com>
2017-01-29 18:02:02 +0000

    added time to save and update methods

--- Ticket.rb
+++ Ticket.rb
@@ -8,17 +8,17 @@ class Ticket
     @id = options['id'].to_i
     @customer_id = options['customer_id'].to_i
     @film_id = options['film_id'].to_i
-    @time = options['time'].to_f
+    @time = options['time']
   end
 
   def save()
-    sql = "INSERT INTO tickets (customer_id, film_id) VALUES (#{@customer_id}, #{@film_id}) RETURNING id;"
+    sql = "INSERT INTO tickets (customer_id, film_id, time) VALUES (#{@customer_id}, #{@film_id}, '#{@time}') RETURNING id;"
     ticket = SqlRunner.run(sql).first
     @id = ticket['id'].to_i
   end
 
   def update()
-    sql = "UPDATE tickets SET (customer_id, film_id) = (#{@customer_id}, #{@film_id}) WHERE id = #{@id};"
+    sql = "UPDATE tickets SET (customer_id, film_id, time) = (#{@customer_id}, #{@film_id}, #{@time}) WHERE id = #{@id};"
     SqlRunner.run(sql)
   end
 
@@ -40,7 +40,7 @@ class Ticket
 
 # most tickets sold
   def self.most_popular_time(time)
-    
+
   end
 
 
